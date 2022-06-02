4bb28d6240e3b16f49b528bdf25357e3b93abcf6
HamzaZafarBhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-06-02 16:33:44 +0500

    vasdv

--- dgda-html-box/assets/js/custom.js
+++ dgda-html-box/assets/js/custom.js
@@ -1,3 +1,29 @@
+function splash_video_fade() {
+  gsap.to(".dg_vw_screen", {
+    display: 'none',
+    duration: 1
+  });
+  gsap.to(".logo_diriyah", {
+    padding: '120px',
+    top: '0',
+    left: '50%',
+    width: '50%',
+    duration: 2,
+    delay: 1
+  })
+  gsap.to(".logo_diriyah", {
+    left: '0',
+    duration: 2,
+    delay: 3
+  });
+  gsap.from(".dg_rs_screen", {
+    opacity: 1,
+    x: 3000,
+    duration: 2,
+    delay: 2
+  });
+
+}
 function move_left() {
   gsap.to(".dg_rs_screen", {
     opacity: 0,
@@ -176,7 +202,9 @@ function move_start_basementfloors_right() {
   });
 }
 
-function move_showmodelactive_left() {
+function move_showmodelactive(elem) {
+  var nextSibling = $(elem).parent().next();
+  nextSibling.attr('data-fromdir', 'right')
   gsap.to(".dg_rs_screen", {
     opacity: 0,
     x: -6000,
@@ -202,34 +230,62 @@ function move_showmodelactive_left() {
     duration: 2,
   });
 }
-function move_videowallsactive_left() {
-  gsap.to(".dg_lights_screen", {
-    opacity: 0,
-    x: -3000,
-    duration: 2
-  });
-  gsap.to(".dg_afrs_screen", {
-    opacity: 0,
-    x: -3000,
-    duration: 2
-  });
-  gsap.fromTo(".dg_vwalls_screen", {
-    opacity: 0,
-    x: 3000,
-    duration: 2,
-    visibility: 'hidden',
-  }, {
-    opacity: 1,
-    x: 0,
-    duration: 2,
-    visibility: 'visible',
-  });
+function move_videowallsactive(elem) {
+  var fromDir = elem.getAttribute('data-fromDir');
+  console.log(fromDir)
+  if(fromDir == 'right') {
+    gsap.to(".dg_lights_screen", {
+      opacity: 0,
+      x: -3000,
+      duration: 2
+    });
+    gsap.to(".dg_afrs_screen", {
+      opacity: 0,
+      x: -3000,
+      duration: 2
+    });
+    gsap.fromTo(".dg_vwalls_screen", {
+      opacity: 0,
+      x: 3000,
+      duration: 2,
+      visibility: 'hidden',
+    }, {
+      opacity: 1,
+      x: 0,
+      duration: 2,
+      visibility: 'visible',
+    });
+  } else {
+    gsap.to(".dg_lights_screen", {
+      opacity: 0,
+      x: -3000,
+      duration: 2
+    });
+    gsap.to(".dg_afrs_screen", {
+      opacity: 0,
+      x: -3000,
+      duration: 2
+    });
+    gsap.fromTo(".dg_vwalls_screen", {
+      opacity: 0,
+      x: 3000,
+      duration: 2,
+      visibility: 'hidden',
+    }, {
+      opacity: 1,
+      x: 0,
+      duration: 2,
+      visibility: 'visible',
+    });
+  }
 }
 function move_vwalls_right() {
-  
+
 }
 
-function move_lightsactive_left() {
+function move_lightsactive(elem) {
+  var prevSibling = $(elem).parent().prev();
+  prevSibling.attr('data-fromdir', 'left')
   gsap.to(".dg_vwalls_screen", {
     opacity: 0,
     x: -3000,
@@ -262,29 +318,29 @@ function move_lightsactive_left() {
 $(document).ready(function () {
 
 
-  $("#main_video_btn").click(function (e) {
-    e.preventDefault();
-    $(".dg_vw_screen").fadeOut();
-    $("#main_video").fadeOut();
-    $('.load_log_col .logo_diriyah').addClass('startanim');
-    $('.dg_rs_row').addClass('firstsecanim');
-    $('.load_log_col').addClass('logowrapanim');
-    // $(".dg_vw_screen").fadeOut("slow");
-    // $(".dg_vw_screen").fadeOut(3000);
-    // $("#main_video").fadeOut("slow");
-    // $("#main_video").fadeOut(3000);
-    $("#dg_rs_screen").fadeIn();
-    $("#dg_rs_screen").fadeIn("slow");
-    $("#dg_rs_screen").fadeIn(5000);
-  });
-
-  // $("#diriyah-room").click(function(e){
+  // $("#main_video_btn").click(function (e) {
   //   e.preventDefault();
-  //   $('#dg_rs_screen').addClass('firstsecanimOut');
-  //   $('#dg_afrs_screen').addClass('firstsec');
-  //   $("#header").fadeIn();
-  //   $("#header").fadeIn("slow");
+  //   $(".dg_vw_screen").fadeOut();
+  //   $("#main_video").fadeOut();
+  //   $('.load_log_col .logo_diriyah').addClass('startanim');
+  //   $('.dg_rs_row').addClass('firstsecanim');
+  //   $('.load_log_col').addClass('logowrapanim');
+  //   // $(".dg_vw_screen").fadeOut("slow");
+  //   // $(".dg_vw_screen").fadeOut(3000);
+  //   // $("#main_video").fadeOut("slow");
+  //   // $("#main_video").fadeOut(3000);
+  //   $("#dg_rs_screen").fadeIn();
+  //   $("#dg_rs_screen").fadeIn("slow");
+  //   $("#dg_rs_screen").fadeIn(5000);
   // });
+
+  $("#diriyah-room").click(function(e){
+    e.preventDefault();
+    $('#dg_rs_screen').addClass('firstsecanimOut');
+    $('#dg_afrs_screen').addClass('firstsec');
+    $("#header").fadeIn();
+    $("#header").fadeIn("slow");
+  });
 
   // Added features section for desktop
   $(".ml_dg_anchor1_wrap").click(function () {
