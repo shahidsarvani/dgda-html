405dc6ff9af96b875f5ef1aebd30e4e2f63ad91b
Mian Muhammad Bilal <bilalcomsis3@gmail.com>
2024-09-18 17:02:04 +0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$().ready(function () {
+$(document).ready(function () {
   var defaultAnimations = [
     "fade-in",
     "slide-left",
@@ -6,6 +6,35 @@ $().ready(function () {
     "slide-up",
     "slide-down",
   ];
+
+  // Establish WebSocket connection
+  var socket = new WebSocket("ws://localhost:8080");
+
+  socket.onopen = function () {
+    console.log("WebSocket connection established");
+  };
+
+  socket.onerror = function (error) {
+    console.log("WebSocket Error: " + error);
+  };
+
+  // Variable to prevent triggering slide change from receiving message
+  var isReceiving = false;
+
+  socket.onmessage = function (event) {
+    console.log("Received raw message:", event.data); // Log the raw data
+    try {
+      var receivedData = JSON.parse(event.data);
+      console.log("Parsed message:", receivedData); // Log the parsed data
+      if (receivedData.action === "slideChange" && !isReceiving) {
+        isReceiving = true;
+        $(".slick-carousel").slick("slickGoTo", receivedData.slideIndex);
+        isReceiving = false;
+      }
+    } catch (error) {
+      console.error("Error parsing message:", error);
+    }
+  };
 
   $(".slick-carousel").slick({
     arrows: true,
@@ -16,18 +45,52 @@ $().ready(function () {
     speed: 500,
     cssEase: "linear",
     infinite: false,
+    draggable: false,
   });
 
   $(".slick-carousel").on(
     "beforeChange",
     function (event, slick, currentSlide, nextSlide) {
-      var currentElement = $(slick.$slides[currentSlide]);
-      var nextElement = $(slick.$slides[nextSlide]);
-      currentElement.removeClass(defaultAnimations.join(" "));
-      var animationClass =
-        $(nextElement).data("animation") ||
-        defaultAnimations[nextSlide % defaultAnimations.length];
-      nextElement.addClass(animationClass);
+      // console.log("beforeChange event triggered", currentSlide, nextSlide);
+      // console.log("isReceiving:", isReceiving);
+      if (!isReceiving) {
+        var currentElement = $(slick.$slides[currentSlide]);
+        var nextElement = $(slick.$slides[nextSlide]);
+        currentElement.removeClass(defaultAnimations.join(" "));
+        var animationClass =
+          $(nextElement).data("animation") ||
+          defaultAnimations[nextSlide % defaultAnimations.length];
+        nextElement.addClass(animationClass);
+
+        // Broadcast the slide change to the WebSocket server
+        socket.send(
+          JSON.stringify({ action: "slideChange", slideIndex: nextSlide })
+        );
+        console.log(`your current slide is ${currentSlide}`);
+        // Make the API call to update the server with the slide change
+        console.log(`Making API call to update slide ${nextSlide}`);
+        fetch(`http://localhost:3001/api/room/2/play_pres/${nextSlide + 1}`)
+          .then((response) => {
+            console.log("Received response from API:", response);
+            if (!response.ok) {
+              return response.text().then((text) => {
+                throw new Error(
+                  `Network response was not ok: ${response.status} ${response.statusText} - ${text}`
+                );
+              });
+            }
+            return response.json();
+          })
+          .then((data) => {
+            console.log("API response data:", data);
+          })
+          .catch((error) => {
+            console.error(
+              "There was a problem with the fetch operation:",
+              error
+            );
+          });
+      }
     }
   );
 
@@ -39,6 +102,30 @@ $().ready(function () {
   $("a[data-slide]").click(function (e) {
     e.preventDefault();
     var slideno = $(this).data("slide");
-    $(".slick-carousel").slick("slickGoTo", slideno - 1);
+    $(".slick-carousel").slick("slickGoTo", slideno);
+
+    // Broadcast the slide change to the WebSocket server
+    if (!isReceiving) {
+      socket.send(
+        JSON.stringify({ action: "slideChange", slideIndex: slideno })
+      );
+
+      // Make the API call to update the server with the slide change
+      console.log(`Making API call to update slide ${slideno}`); // Log before fetch
+      fetch(`http://localhost:3001/api/room/2/play_pres/${slideno}`)
+        .then((response) => {
+          console.log("Received response from API:", response); // Log the response
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("API response data:", data); // Handle API response if needed
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    }
   });
 });
---
+++ node_files/client/index.js
@@ -0,0 +1,302 @@
+require("dotenv").config();
+const SERVER_HOST = process.env.SERVER_HOST;
+const SERVER_PORT = process.env.SERVER_PORT;
+const url = `http://${SERVER_HOST}:${SERVER_PORT}`;
+var VLC = require("vlc-simple-player");
+const { io } = require("socket.io-client");
+let player;
+let volume = 0;
+let prev_volume = 0;
+let is_muted = 0;
+// let do_empty = 1;
+
+var basePlaylistID = 3;
+// var defaultVideo = '';
+
+const socket = io(url, {
+  query: {
+    room_id: process.env.ROOM_ID,
+    is_projector: process.env.IS_PROJECTOR,
+  },
+});
+
+// var sockets = new WebSocket("ws://localhost:3001");
+
+// sockets.onopen = function () {
+//   console.log("WebSocket connection established");
+// };
+
+// sockets.onerror = function (error) {
+//   console.log("WebSocket Error: " + error);
+// };
+// sockets.on("connect", function (socket) {
+//   console.log("Connected!");
+// });
+socket.on(process.env.CHANGE_DEFAULT_VIDEO_EVENT, (msg) => {
+  if (msg && msg.length) {
+    console.log(msg);
+    // defaultVideo = msg
+    default_play_video(msg);
+  }
+});
+socket.on(process.env.CHANGE_VIDEO_EVENT, (msg) => {
+  if (msg && msg.length) {
+    console.log(msg);
+    change_video(msg);
+  }
+});
+// socket.on(process.env.CHANGE_ZONE_VIDEO_EVENT, (msg) => {
+//   if (msg && msg.length) {
+//     console.log(msg);
+//     change_zone_video(msg);
+//   }
+// });
+
+socket.on(process.env.CHANGE_ZONE_VIDEO_EVENT, (msg) => {
+  if (msg && msg.length) {
+    console.log("Slide change received:", msg);
+    change_zone_video(msg);
+  }
+});
+
+function getDefaultArgs() {
+  options = process.env.VLCCOMMANDS.split(",");
+  const search = ";";
+  const replacer = new RegExp(search, "g");
+  var args = [];
+  options.forEach((option) => {
+    args.push(option.replace(replacer, ","));
+  });
+  var defaultArguments = {
+    arguments: args,
+  };
+  // console.log(defaultArguments.arguments);
+  return defaultArguments;
+}
+
+function default_play_video(msg) {
+  console.log("default");
+  if (!player) player = new VLC(msg, getDefaultArgs());
+  else addItem(msg);
+
+  if (process.env.IS_PROJECTOR == 0) {
+    player.on("statuschange", (error, status) => {
+      // console.log(status)
+      if (status) {
+        volume = status.volume;
+      }
+    });
+  }
+}
+
+function addItem(videoName) {
+  player.request("/requests/status.json?command=pl_empty", () => {});
+  player.request(
+    "/requests/status.json?command=in_enqueue&input=" + encodeURI(videoName),
+    () => {}
+  );
+  console.log(basePlaylistID);
+  player.request(
+    "/requests/status.json?command=pl_delete&id=" + basePlaylistID,
+    () => {}
+  );
+  player.request(
+    "/requests/status.json?command=pl_play&id=" + basePlaylistID++,
+    () => {}
+  );
+}
+
+// function play_video(video) {
+//   console.log(video);
+//   player = new VLC(video[0].toString(), getDefaultArgs());
+//   if (process.env.IS_PROJECTOR == 0) {
+//     player.on("statuschange", (error, status) => {
+//       console.log(status);
+//       volume = status.volume;
+//       if (error) return console.error(error);
+//       console.log("Times: " + (status.time + 1) + "/" + status.length);
+//       if (status.time == status.length - 1) {
+//         console.log("time completed");
+//         socket.emit("default_video", {
+//           room_id: process.env.ROOM_ID,
+//           lang: video[1],
+//         });
+//       }
+//     });
+//   }
+// }
+function play_video(video) {
+  const [videoPath, lang] = video;
+  const duration = 1800; // Set duration to 1800 seconds (30 minutes)
+  const args = getDefaultArgs();
+  args.arguments.push(`--image-duration=${duration}`);
+  player = new VLC(videoPath, args);
+
+  player.on("statuschange", (error, status) => {
+    if (error) return console.error(error);
+    if (status.time == status.length - 1) {
+      console.log("Slide duration ended, sending event to server");
+      socket.emit("default_video", { room_id: process.env.ROOM_ID, lang });
+    }
+  });
+}
+
+function change_video(video) {
+  console.log("change video");
+  console.log(video);
+  // do_empty = 1
+  if (!player) {
+    console.log("new video");
+    play_video(video);
+  } else {
+    console.log("running video");
+
+    addItem(video[0]);
+
+    if (process.env.IS_PROJECTOR == 0) {
+      player.on("statuschange", (error, status) => {
+        // console.log(status)
+        volume = status.volume;
+        if (error) return console.error(error);
+        console.log("meow");
+        console.log("Times: " + (status.time + 1) + "/" + status.length);
+        if (status.time == status.length - 1) {
+          console.log("time completed");
+          socket.emit("default_video", {
+            room_id: process.env.ROOM_ID,
+            lang: video[1],
+          });
+        }
+      });
+    }
+  }
+}
+
+// function change_zone_video(video) {
+//   console.log("change zone video");
+//   console.log(video);
+//   if (!player) {
+//     console.log("new zone video");
+//     play_video(video);
+//   } else {
+//     console.log("running zone video");
+//     addItem(video[0]);
+//   }
+// }
+
+function change_zone_video(video) {
+  if (!player) {
+    console.log("Initializing VLC player with slide:", video[0]);
+    play_video(video);
+  } else {
+    console.log("Updating VLC player with new slide:", video[0]);
+    addItem(video[0]);
+  }
+}
+
+socket.on(process.env.VIDEO_EVENTS, (msg) => {
+  console.log(msg);
+  // console.log(msg[1])
+  // // return;
+  // var volume = 0
+  // if (typeof msg == 'object') {
+  //   volume = msg[1]
+  //   msg = msg[0]
+  // }
+  switch (msg) {
+    case "play":
+      if (player)
+        player.request("/requests/status.json?command=pl_pause", () => {});
+      break;
+    case "pause":
+      if (player)
+        player.request("/requests/status.json?command=pl_pause", () => {});
+      break;
+    case "forward":
+      if (player)
+        player.request("/requests/status.json?command=seek&val=+10s", () => {});
+      break;
+    case "back":
+      if (player)
+        player.request("/requests/status.json?command=seek&val=-10s", () => {});
+      break;
+    case "stop":
+      if (player)
+        player.request("/requests/status.json?command=pl_stop", () => {});
+      socket.emit("default_video", {
+        room_id: process.env.ROOM_ID,
+        lang: "en",
+      });
+      break;
+    case "up":
+      console.log("volume up command received");
+      if (process.env.IS_PROJECTOR == 0) {
+        console.log("volume up command received");
+        volume += 20;
+        console.log(volume);
+        if (player)
+          player.request(
+            "/requests/status.json?command=volume&val=" + volume,
+            () => {}
+          );
+      }
+      break;
+    case "down":
+      if (process.env.IS_PROJECTOR == 0) {
+        console.log("volume down command received");
+        volume -= 20;
+        console.log(volume);
+        if (player)
+          player.request(
+            "/requests/status.json?command=volume&val=" + volume,
+            () => {}
+          );
+      }
+      break;
+    case "mute":
+      if (process.env.IS_PROJECTOR == 0) {
+        console.log(is_muted);
+        if (is_muted == 1) {
+          volume = prev_volume;
+          is_muted = 0;
+        } else {
+          prev_volume = volume;
+          volume = 0;
+          is_muted = 1;
+        }
+        console.log(prev_volume);
+        console.log(volume);
+        if (player)
+          player.request(
+            "/requests/status.json?command=volume&val=" + volume,
+            () => {}
+          );
+      }
+      break;
+    // case "unmute":
+    //   if (process.env.IS_PROJECTOR == 0) {
+    //     volume = prev_volume
+    //     console.log(prev_volume)
+    //     console.log(volume)
+    //     if (player) player.request('/requests/status.json?command=volume&val=' + volume, () => { })
+    //   }
+    //   break;
+    default:
+      console.log("Error!");
+      break;
+  }
+});
+
+// client.on('change_video_wsw', (data) => {
+//   console.log(data)
+//   play_video(data)
+// })
+
+// // Add a 'close' event handler for the client socket
+// client.on('close', () => {
+//   console.log('Client closed');
+// });
+
+// client.on('error', (err) => {
+//   console.error(err);
+// });
